e862cfdd0760ae54cb12c7ef214f85c094d7b2b5
mitchplass <mitchellplass@gmail.com>
2025-05-02 17:00:52 +1000

    Basic setup

---
+++ iOSDevAssignment3/iOSDevAssignment3/HomeScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSDevAssignment3
+//
+//  Created by Mitchell Plass on 2/5/2025.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        VStack {
+            Text("TripSync")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
