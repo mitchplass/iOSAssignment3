caa03e9d9169e3671b7177c9d773afd0f14bbd7e
mitchplass <mitchellplass@gmail.com>
2025-05-02 16:52:38 +1000

    first

---
+++ iOSDevAssignment3/iOSDevAssignment3/iOSDevAssignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevAssignment3App.swift
+//  iOSDevAssignment3
+//
+//  Created by Mitchell Plass on 2/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevAssignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
