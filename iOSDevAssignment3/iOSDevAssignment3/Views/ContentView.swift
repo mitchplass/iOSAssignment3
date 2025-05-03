17a4d8117aba8e3dc86d5707b0dafc02bb15f1c5
baileytucker1259 <bailey.tucker1259@gmail.com>
2025-05-03 06:22:20 -0700

    Revert "Added inital models and view models and views for basic trips"
    This reverts commit 5841033a6242dbfa56c5fb577d8bd036fa4b540e.

--- iOSDevAssignment3/iOSDevAssignment3/Views/ContentView.swift
+++ iOSDevAssignment3/iOSDevAssignment3/Views/ContentView.swift
@@ -2,66 +2,20 @@
 //  ContentView.swift
 //  iOSDevAssignment3
 //
-//  Created by baileyt on 3/5/25.
+//  Created by Mitchell Plass on 2/5/2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var tripViewModel: TripViewModel
-    @State private var showingNewTripView = false
-    
     var body: some View {
-        ZStack {
-            if tripViewModel.currentTrip != nil {
-                VStack {
-                    HStack {
-                        Button("Back to Trips") {
-                            tripViewModel.currentTrip = nil
-                        }
-                        .padding(.leading)
-                        
-                        Spacer()
-                    }
-                    .padding(.top)
-                    
-                    TripDetailView(trip: tripViewModel.currentTrip!)
-                        .padding(.top, 8)
-                }
-            } else {
-                VStack {
-                    HStack {
-                        Text("TripSync")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(.leading)
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            showingNewTripView = true
-                        }) {
-                            Image(systemName: "plus")
-                                .font(.title2)
-                        }
-                        .padding(.trailing)
-                    }
-                    .padding(.top)
-                    
-                    TripListView()
-                        .padding(.top, 8)
-                }
-                .sheet(isPresented: $showingNewTripView) {
-                    NewTripView(showingNewTripView: $showingNewTripView)
-                }
-            }
+        VStack {
+            HomeScreenView()
         }
+        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .environmentObject(TripViewModel())
-    }
+#Preview {
+    ContentView()
 }
